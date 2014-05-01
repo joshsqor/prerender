e15e966ae2ccc2ce0963159d3341872f70bbd52e
alfonsoh <alfonsohooker@gmail.com>
2014-05-01 13:57:31 -0700

    adding new relic monitoring and fixing worker threads

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
+require('newrelic');
 var prerender = require('./lib')
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 40,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT || 30000
 });
 
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
 server.start();
