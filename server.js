f1686e3abb609e0c17f47f3e46b611608c7eb303
Josh Ballesteros <josh.ballesteros@sqor.com>
2014-05-05 10:19:42 -0700

    Update server.js
    Updating server.js to reduce number of threads to prevent server from crashing.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('newrelic');
 var prerender = require('./lib')
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 40,
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 10,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT || 30000
